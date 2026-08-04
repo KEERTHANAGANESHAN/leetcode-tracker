c388c8bdfb4746fd0ad0cc4eb3df150de5d85fb3
KEERTHANAGANESHAN <keerthanaganeshan49@gmail.com>
2026-08-04 14:38:30 +0530

    File updated at 8/4/2026, 2:38:29 PM

--- 1291-SequentialDigits/1291-SequentialDigits.java
+++ 1291-SequentialDigits/1291-SequentialDigits.java
@@ -1,18 +1,22 @@
-// Last updated: 8/4/2026, 2:30:01 PM
+// Last updated: 8/4/2026, 2:38:29 PM
 1class Solution {
-2    public int maxProduct(int n) {
-3        int max1 = -1 , max2 = -1;
-4        while(n != 0){
-5            int rem = n % 10;
-6            if(max1 <= rem){
-7                max2 = max1;
-8                max1 = rem;
-9            }
-10            else if(max2 < rem){
-11                max2 = rem;
-12            }
-13            n /= 10;
-14        }
-15        return max1 * max2;
-16    }
-17}
+2    private int gcd(int a, int b) {
+3        while (b != 0) {
+4            int temp = b;
+5            b = a % b;
+6            a = temp;
+7        }
+8        return a;
+9    }
+10    public int findGCD(int[] nums) {
+11        int min = nums[0];
+12        int max = nums[0];
+13        for (int num : nums) {
+14            min = Math.min(min, num);
+15            max = Math.max(max, num);
+16        }
+17        return gcd(min, max);
+18    }
+19}
+20
+21
