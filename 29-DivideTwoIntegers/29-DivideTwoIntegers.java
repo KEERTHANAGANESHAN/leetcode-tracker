eed65f779996e56f2522d2c8751ab26bbf9462cb
KEERTHANAGANESHAN <keerthanaganeshan49@gmail.com>
2026-08-07 11:43:46 +0530

    File updated at 8/7/2026, 11:43:47 AM

--- 29-DivideTwoIntegers/29-DivideTwoIntegers.java
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.java
@@ -1,22 +1,21 @@
-// Last updated: 7/9/2026, 9:28:20 AM
-class Solution {
-    public int divide(int dividend, int divisor) {
-        if (dividend == Integer.MIN_VALUE && divisor == -1) {
-            return Integer.MAX_VALUE;
-        }
-        long a = Math.abs((long) dividend);
-        long b = Math.abs((long) divisor);
-        int result = 0;
-
-        while (a >= b) {
-            long temp = b, multiple = 1;
-            while (a >= (temp << 1)) {
-                temp <<= 1;
-                multiple <<= 1;
-            }
-            a -= temp;
-            result += multiple;
-        }
-        return ((dividend > 0) == (divisor > 0)) ? result : -result;
-    }
-}
+// Last updated: 8/7/2026, 11:43:47 AM
+1class Solution {
+2    public int divide(int dividend, int divisor) {
+3        if (dividend == Integer.MIN_VALUE && divisor == -1)
+4            return Integer.MAX_VALUE;
+5        long a = Math.abs((long) dividend);
+6        long b = Math.abs((long) divisor);
+7        int ans = 0;
+8        while (a >= b) {
+9            int shift = 0;
+10            while (a >= (b << (shift + 1))) {
+11                shift++;
+12            }
+13            a -= (b << shift);
+14            ans += (1 << shift);
+15        }
+16        if ((dividend > 0) ^ (divisor > 0))
+17            ans = -ans;
+18        return ans;
+19    }
+20}
